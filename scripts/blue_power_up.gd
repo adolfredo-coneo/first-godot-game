0f4e88b79e5a08b5f195b8d8e3a512ae065ce3b9
Adolfredo Coneo <ado.coneo@gmail.com>
2024-08-10 14:30:45 -0500

    add music, coin and poweup sound effects

--- scripts/blue_power_up.gd
+++ scripts/blue_power_up.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
 var bpuTotal : int = 0
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
 	bpuTotal +=1
+	animation_player.play("blue_powerup")
 	print(bpuTotal)
-	queue_free()
